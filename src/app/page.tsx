7f8ca40aed7103bff78265bb9808244e00da7a32
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-07-29 21:28:34 +1000

    connect to supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import Header from "@/components/Header";
 import Tabs from "@/components/Tabs";
-import { TabGroup } from '@headlessui/react';
+import { createClient } from "@/utils/supabaseClient";
+import { TabGroup } from "@headlessui/react";
 
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+  const selectedColumns: Array<string> = [
+    "院校名称",
+    "科类",
+    "专业名称",
+  ];
+  let { data: 历史本科提前批, error } = await supabase
+    .from("历史本科提前批")
+    .select(selectedColumns.length > 0 ? selectedColumns.join(","): "*")
+    .range(0, 9);
+    
   return (
     <TabGroup>
       <Header />
@@ -13,52 +25,37 @@ export default function Home() {
         <table className="min-w-full bg-white">
           <thead>
             <tr className="w-full bg-blue-500 text-white">
-              <th className="py-2 px-4">Header 1</th>
-              <th className="py-2 px-4">Header 2</th>
-              <th className="py-2 px-4">Header 3</th>
+              {
+                selectedColumns.map((key) => {
+                  return <th className="py-2 px-4" key={crypto.randomUUID()}>{key}</th>
+                })
+              }
               <th className="py-2 px-4">Operations</th>
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-gray-100">
-              <td className="border px-4 py-2">Data 1</td>
-              <td className="border px-4 py-2">Data 2</td>
-              <td className="border px-4 py-2">Data 3</td>
-              <td className="border px-4 py-2">
-                <button className="py-1 px-2 text-white bg-green-500 hover:bg-green-700 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
-                  Edit
-                </button>
-                <button className="py-1 px-2 text-white bg-red-500 hover:bg-red-700 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">
-                  Delete
-                </button>
-              </td>
-            </tr>
-            <tr className="bg-white">
-              <td className="border px-4 py-2">Data 4</td>
-              <td className="border px-4 py-2">Data 5</td>
-              <td className="border px-4 py-2">Data 6</td>
-              <td className="border px-4 py-2">
-                <button className="py-1 px-2 text-white bg-green-500 hover:bg-green-700 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
-                  Edit
-                </button>
-                <button className="py-1 px-2 text-white bg-red-500 hover:bg-red-700 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">
-                  Delete
-                </button>
-              </td>
-            </tr>
-            <tr className="bg-gray-100">
-              <td className="border px-4 py-2">Data 7</td>
-              <td className="border px-4 py-2">Data 8</td>
-              <td className="border px-4 py-2">Data 9</td>
-              <td className="border px-4 py-2">
-                <button className="py-1 px-2 text-white bg-green-500 hover:bg-green-700 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
-                  Edit
-                </button>
-                <button className="py-1 px-2 text-white bg-red-500 hover:bg-red-700 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">
-                  Delete
-                </button>
-              </td>
-            </tr>
+            {error && <tr><td colSpan={selectedColumns.length + 1}>{error.message}</td></tr>}
+            {历史本科提前批?.map((row) => {
+              return (
+                <tr className="bg-gray-100" key={crypto.randomUUID()}>
+                  {
+                    selectedColumns.map((value, idx) => {
+                      return <td className="border px-4 py-2" key={crypto.randomUUID()}>{row[value]}
+                        </td>
+                    })
+                  }
+                  <td className="border px-4 py-2">
+                    <button className="py-1 px-2 text-white bg-green-500 hover:bg-green-700 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
+                      Edit
+                    </button>
+                    <button className="py-1 px-2 text-white bg-red-500 hover:bg-red-700 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            }
+            )}
           </tbody>
         </table>
       </div>
