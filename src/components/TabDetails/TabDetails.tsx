6943a2d607a39dcccb810ae5bfc5d03346e9489d
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-08-04 18:01:22 +1000

    add tab details

---
+++ src/components/TabDetails/TabDetails.tsx
@@ -0,0 +1,13 @@
+import { TabPanel, TabPanels } from '@headlessui/react';
+
+
+function TabDetails() {
+    return (
+      <TabPanels>
+        <TabPanel>1</TabPanel>
+        <TabPanel>2</TabPanel>
+        <TabPanel>3</TabPanel>
+      </TabPanels>
+    );
+};
+export default TabDetails;
